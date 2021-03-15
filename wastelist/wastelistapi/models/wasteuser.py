4a011e33a1996ec3d166d44acf6155c8b66af059
ericaphillips <ericaphill@gmail.com>
2021-03-15 15:30:10 -0500

    added fixtures

--- wastelist/wastelistapi/models/wasteuser.py
+++ wastelist/wastelistapi/models/wasteuser.py
@@ -6,4 +6,4 @@ class WasteUser(models.Model):
     user = models.OneToOneField(User, on_delete=CASCADE)
     phone = models.IntegerField()
     zipcode = models.IntegerField()
-    pharmacyId = models.ForeignKey("Pharmacy", on_delete=CASCADE)
+    pharmacy = models.ForeignKey("Pharmacy", on_delete=CASCADE, null = True)
